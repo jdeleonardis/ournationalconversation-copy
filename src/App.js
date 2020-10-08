6729ea6387ca3c4b7a2b1f85f601772f46295875
Rose <rosier101@gmail.com>
2020-10-08 19:52:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
