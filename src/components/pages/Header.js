1396f238e8f58d90f444b4632814b8cf57355e2c
rosier101 <rosier101@gmail.com>
2020-10-08 19:31:03 -0400

    Add files via upload
    Header.js file created for the header and navbar

---
+++ src/components/pages/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  
+
+  render(){
+    return(
+      <div>
+        <p>This is where Header shows up</p>
+      </div>
+    )
+  }
+}
+
+export default Header;
