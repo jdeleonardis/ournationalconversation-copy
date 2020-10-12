842501eabfe6cbdf0b222f13effb8619a5832aee
ANS <36783010+answebdev@users.noreply.github.com>
2020-10-12 16:50:46 -0700

    updated validation and added RegEx

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -14,7 +14,134 @@ import {
 } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
+const initialState = {
+  userName: '',
+  email: '',
+  password: '',
+  userNameError: '',
+  emailError: '',
+  passwordError: '',
+};
+
+// Include error messages displayed to user
+// const initialState = {
+//   userName: '',
+//   email: '',
+//   password: '',
+//   userNameError: '',
+//   emailError: '',
+//   passwordError: '',
+// };
+
 export class Login extends Component {
+  state = initialState;
+
+  // Handle change of the inputs
+  handleChange = (event) => {
+    const isCheckbox = event.target.type === 'checkbox';
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value,
+    });
+  };
+
+  validate = () => {
+    // let userNameError = '';
+    let emailError = '';
+    let passwordError = '';
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    // General Email Regex (RFC 5322 Official Standard)
+    // /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+    // if (!this.state.userName) {
+    //   userNameError = 'name cannot be blank';
+    // }
+
+    // if (!this.state.email) {
+    //   emailError = 'Please enter your email address or username.';
+    // }
+
+    // Email validation: basic version
+    // if (!this.state.email.includes('@')) {
+    //   emailError = 'Please enter a valid email.';
+    // }
+
+    // RegEx version
+    if (!this.state.email.match(emailRegex)) {
+      emailError = 'Please enter a valid email.';
+    } else {
+      console.log('Email is valid');
+    }
+
+    if (emailError) {
+      this.setState({ emailError });
+      return false;
+    }
+
+    // RegEx version: password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
+    if (!this.state.password.match(passwordRegex)) {
+      passwordError = 'Please enter a valid password.';
+    } else {
+      console.log(
+        'Password is valid (between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter)'
+      );
+    }
+
+    // Password validation: basic version
+    // console.log(this.state.password);
+    // if (this.state.password < 6) {
+    //   passwordError = 'Invalid password. Please try again.';
+    // }
+
+    // if (!this.state.password) {
+    //   passwordError = 'Password is required.';
+    // }
+
+    if (passwordError) {
+      this.setState({ passwordError });
+      return false;
+    }
+
+    // if (this.state.email === '' || this.state.password === '') {
+    //   userNameError = 'Please fill in all fields';
+    // }
+
+    // if (userNameError) {
+    //   this.setState({ userNameError });
+    //   return false;
+    // }
+
+    // if (!this.state.password.length > 6) {
+    //   passwordError = 'Please enter a valid password';
+    // }
+
+    // if (emailError || passwordError) {
+    //   this.setState({ emailError, passwordError });
+    //   return false;
+    // }
+
+    // if (this.state.password.length < 6) {
+    //   passwordError = 'Please enter a valid email';
+    // }
+
+    return true;
+  };
+
+  // Submit the state
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      // console.log(this.state);
+
+      // Clear form if submission is valid
+      this.setState(initialState);
+    }
+  };
+
   render() {
     return (
       <Fragment>
@@ -70,14 +197,32 @@ export class Login extends Component {
                           </span>
                         </div>
                       </Card.Subtitle>
-                      <Form>
+                      <Form onSubmit={this.handleSubmit}>
                         <Form.Group controlId='formBasicEmail'>
                           <Form.Label>USERNAME OR EMAIL</Form.Label>
-                          <Form.Control type='email' required />
+                          <Form.Control
+                            // type='email'
+                            name='email'
+                            // placeholder='Username or Email'
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                          />
+                          <div style={{ fontSize: 12, color: 'red' }}>
+                            {this.state.emailError}
+                          </div>
                         </Form.Group>
                         <Form.Group controlId='formBasicPassword'>
                           <Form.Label>PASSWORD</Form.Label>
-                          <Form.Control type='password' required />
+                          <Form.Control
+                            type='password'
+                            name='password'
+                            // placeholder='password'
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                          />
+                          <div style={{ fontSize: 12, color: 'red' }}>
+                            {this.state.passwordError}
+                          </div>
                         </Form.Group>
                         <Form.Group
                           id='remember-group'
