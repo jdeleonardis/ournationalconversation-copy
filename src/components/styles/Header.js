09a869d750f59d87b246c6b51b1f4a4626b952fb
Rose <rosier101@gmail.com>
2020-10-15 18:07:48 -0400

    Add files via upload
    styling the header component

---
+++ src/components/styles/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {MenuItems} from "./MenuItems"
+import './Header.css'
+
+
+class Header extends React.Component {
+  
+
+  render(){
+    return(
+      <nav className = "NavbarItems">
+      <h1 className = "navbar-logo">ONC<i class Name = "fab fa-react"></i>
+      </h1>
+      <div className = "menu-icon"></div>
+      
+     
+       <ul>
+          {MenuItems.map((item, index) =>{
+              return(
+                <li key = {index}>
+                <a className = {MenuItems.cName} 
+                href = {MenuItems.url}>
+                {item.title}
+                </a></li>
+              )
+          })}
+        </ul>
+      
+      </nav>
+    )
+  }
+}
+
+export default Header;
