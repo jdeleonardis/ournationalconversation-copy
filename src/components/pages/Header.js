bdcb361ee7b737d1c5df8a65ed8899b26c6a6899
Rose <rosier101@gmail.com>
2020-10-15 18:03:16 -0400

    Update Header.js
    Updated Header to more efficient code

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {MenuItems} from "./MenuItems"
+import './Header.css'
 
 
 class Header extends React.Component {
@@ -6,16 +8,27 @@ class Header extends React.Component {
 
   render(){
     return(
-      <div className = "Header">
-  <ul>
-  <li><a href="#News">News</a></li>
-  <li><a href="#Opinions">Opinions</a></li>
-  <li><a href="#Solutions">Solutions</a></li>
-</ul>
-      </div>
+      <nav className = "NavbarItems">
+      <h1 className = "navbar-logo">ONC<i class Name = "fab fa-react"></i>
+      </h1>
+      <div className = "menu-icon"></div>
+      
+     
+       <ul>
+          {MenuItems.map((item, index) =>{
+              return(
+                <li key = {index}>
+                <a className = {MenuItems.cName} 
+                href = {MenuItems.url}>
+                {item.title}
+                </a></li>
+              )
+          })}
+        </ul>
+      
+      </nav>
     )
   }
 }
 
-export default Header;
 
