a458f967fe472318f78a84700227b16e4ecbcb3e
rjvarona <rjvarona16@gmail.com>
2020-10-21 13:47:23 -0700

    update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import '../styles/footer.css';
 
 function Footer() {
@@ -30,14 +32,35 @@ function Footer() {
 							<a>Terms of Service</a> <br />
 							<a>Privacy</a> <br />
 						</Col>
-						<Col className="text-left">
-							<a>Terms of Service</a> <br />
-							<a>Privacy</a> <br />
+						<Col className="text-center">
+							<a style={{ fontSize: '50px' }}>ONC</a>
+							<hr
+								style={{
+									backgroundColor: 'white',
+									height: 2,
+									marginTop: 0,
+									marginBottom: 0,
+								}}
+							/>
+							<a style={{ fontSize: '20px' }}>joinONC.com</a> <br />
 						</Col>
 					</Row>
 					<br />
 				</div>
+				<hr
+					style={{
+						backgroundColor: 'white',
+						height: 2,
+						marginTop: 0,
+						marginBottom: 0,
+					}}
+				/>
 			</Container>
+			<Row>
+				<Col> © Our National Conversation | All rights reserved</Col>
+
+				<Col> Follow ONC</Col>
+			</Row>
 		</footer>
 	);
 }
