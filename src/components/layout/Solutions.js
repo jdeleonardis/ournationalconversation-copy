40cefae9d01e5189e600a644684d2a4a97da2f77
rjvarona <rjvarona16@gmail.com>
2020-10-21 13:22:01 -0700

    added hr lines

--- src/components/layout/Solutions.js
+++ src/components/layout/Solutions.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Card, Image, Col, Row, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Comment from './Comments';
-import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 import '../styles/comments.css';
+const ColoredLine = ({ color }) => (
+	<hr
+		style={{
+			backgroundColor: 'red',
+			height: 10,
+		}}
+	/>
+);
 
 function Solutions() {
 	return (
 		<div>
-			<Comment />
-
-			<Comment />
-
-			<Comment />
+			<Card>
+				<Container>
+					<div>Newly Arrived</div> <ColoredLine />
+					<Comment />
+					<br />
+					<Comment />
+					<br />
+					<Comment />
+				</Container>
+			</Card>
 		</div>
 	);
 }
