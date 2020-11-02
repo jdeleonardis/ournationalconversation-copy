ea42b69cf8a7764a6ceccdaf87cf807346911246
Judy Hopps <56984748+judylime@users.noreply.github.com>
2020-11-01 21:59:20 -0800

    Filter Updated

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -9,9 +9,13 @@ import {
   Dropdown,
   Container,
   Row,
-  Col
+  Col,
+  Button
 } from 'react-bootstrap';
 import '../styles/searchBar.css';
+
+import FilterBar from './FilterBar';
+
 class SearchBar extends Component {
   render() {
     return (
@@ -19,35 +23,82 @@ class SearchBar extends Component {
         <Row className='justify-content-md-center'>
           <Navbar bg='light' variant='' fixed='top'>
             <Col className='mb-2' xs lg='2'>
-              <Navbar.Brand href='#home'>
-                <DropdownButton
-                  id='dropdown-basic-button'
-                  title='NEWS FILTERS'
-                ></DropdownButton>
-              </Navbar.Brand>
             </Col>
             <Col md='auto'>
               <Nav className='mr-auto'>
+                <Button href='#'>NEWS FILTERS <i class="fas fa-caret-down"></i></Button> 
                 <Nav.Link href='#'>HOT TOPICS: </Nav.Link>
                 <Nav.Link href='#'>COVID-19 Alert</Nav.Link>
                 <Nav.Link href='#'>Biden VP Pick</Nav.Link>
-                <Nav.Link href='#'>School Reopenings</Nav.Link>
-              </Nav>
-            </Col>
-            <Col xs lg='2'>
-              <Form inline>
+                <Nav.Link href='#'>School Reopenings</Nav.Link><Form inline>
                 <FormControl
                   type='text'
                   placeholder='Search'
                   className='mr-sm-2'
-                />
+                /><i class="fas fa-search"></i>
               </Form>
+              </Nav>
+            </Col>
+            <Col xs lg='2'>
+              
             </Col>
           </Navbar>
         </Row>
+        <Row>
+          <Navbar>
+            
+            <Col md='auto' >
+            <p>Search Opinions:</p>
+            <Form inline>
+                <FormControl
+                  type='text'
+                  placeholder='ENTER KEYWORDS'
+                  className='mr-sm-2'
+                >
+                  </FormControl><i class="fas fa-search"></i>
+              </Form></Col>
+              <hr/>
+
+              <Col md='auto'>
+              <h2>OPINIONS</h2>
+              <p>
+                Newly Arrived  
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i>
+              <i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i><i className="fas fa-minus"  > </i>
+                 Sort by: DEFAULT
+              <i class="fas fa-caret-down"></i>
+              </p>
+              </Col>
+              
+          </Navbar>
+        </Row>
+        <FilterBar/>
       </Container>
     );
   }
 }
+
 
 export default SearchBar;
