7b38b049425c091a74df0c789427bc63aa73ee51
Rose <rosier101@gmail.com>
2020-11-03 19:34:21 -0500

    Update Button.js

--- src/components/layout/Button.js
+++ src/components/layout/Button.js
@@ -26,7 +26,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0]
 
     return (
-        <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`}onClick={onClick}
+        <button className = {`headerbtn ${checkButtonStyle} ${checkButtonSize}`}onClick={onClick}
         type={type}>
             {children}</button>
     )
