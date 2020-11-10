ca3dfa7b96d366c639c53586a692f8bbb5b6130b
ANS <36783010+answebdev@users.noreply.github.com>
2020-11-09 22:43:00 -0800

    resolved merge issues

--- src/components/layout/FilterBar.js
+++ src/components/layout/FilterBar.js
@@ -11,7 +11,6 @@ import {
   Card,
 } from 'react-bootstrap';
 
-
 function handleChevron() {
   document.querySelector('.chev').classList.toggle('fa-chevron-down');
   document.querySelector('.chev').classList.toggle('fa-chevron-up');
@@ -164,262 +163,268 @@ const FilterBar = () => {
     <div>
       <h2>Filter by:</h2>
       <div>
-      <a onClick={handleChevron} id='topic' href='#demo' data-toggle='collapse'>
-        <i class='chev fas fa-chevron-down'></i>&nbsp;&nbsp;TOPIC
-      </a>
+        <a
+          onClick={handleChevron}
+          id='topic'
+          href='#demo'
+          data-toggle='collapse'
+        >
+          <i class='chev fas fa-chevron-down'></i>&nbsp;&nbsp;TOPIC
+        </a>
 
-      <div
-        style={{ marginTop: '10px', marginLeft: '20px' }}
-        id='demo'
-        className='collapse'
-      >
-        <Form>
-          {/* <h2>Filter by:</h2> */}
-          {/* <p className='filter'>
+        <div
+          style={{ marginTop: '10px', marginLeft: '20px' }}
+          id='demo'
+          className='collapse'
+        >
+          <Form>
+            {/* <h2>Filter by:</h2> */}
+            {/* <p className='filter'>
             <i class='fas fa-chevron-up'></i>
             TOPIC CLEAR
           </p> */}
-          {['radio'].map((type) => (
-            <div key={`custom-${type}`} className='mb-3'>
-              <Form.Check
-                onClick={handleUS}
-                custom
-                type='radio'
-                label='US'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios1'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleWorld}
-                custom
-                type='radio'
-                label='World'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios2'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handlePolitics}
-                custom
-                type='radio'
-                label='Politics'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios3'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleBusiness}
-                custom
-                type='radio'
-                label='Business'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios4'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleTech}
-                custom
-                type='radio'
-                label='Tech'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios5'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleHealth}
-                custom
-                type='radio'
-                label='Health'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios6'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleSciEnv}
-                custom
-                type='radio'
-                label='Science & Environment'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios7'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleMedia}
-                custom
-                type='radio'
-                label='Media'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios8'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handlePolls}
-                custom
-                type='radio'
-                label='Polls'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios9'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleFaith}
-                custom
-                type='radio'
-                label='Faith / Spirituality'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios10'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleFactCheck}
-                custom
-                type='radio'
-                label='Fact Check'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios11'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleSelectAll}
-                custom
-                type='radio'
-                label='Select All'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios12'
-                // className={`custom-${type}`}
-              />
-            </div>
-            
-          ))}
-        </Form>
+            {['radio'].map((type) => (
+              <div key={`custom-${type}`} className='mb-3'>
+                <Form.Check
+                  onClick={handleUS}
+                  custom
+                  type='radio'
+                  label='US'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios1'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleWorld}
+                  custom
+                  type='radio'
+                  label='World'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios2'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handlePolitics}
+                  custom
+                  type='radio'
+                  label='Politics'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios3'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleBusiness}
+                  custom
+                  type='radio'
+                  label='Business'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios4'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleTech}
+                  custom
+                  type='radio'
+                  label='Tech'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios5'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleHealth}
+                  custom
+                  type='radio'
+                  label='Health'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios6'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleSciEnv}
+                  custom
+                  type='radio'
+                  label='Science & Environment'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios7'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleMedia}
+                  custom
+                  type='radio'
+                  label='Media'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios8'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handlePolls}
+                  custom
+                  type='radio'
+                  label='Polls'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios9'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleFaith}
+                  custom
+                  type='radio'
+                  label='Faith / Spirituality'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios10'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleFactCheck}
+                  custom
+                  type='radio'
+                  label='Fact Check'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios11'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleSelectAll}
+                  custom
+                  type='radio'
+                  label='Select All'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios12'
+                  // className={`custom-${type}`}
+                />
+              </div>
+            ))}
+          </Form>
         </div>
-        
       </div>
       <div>
-      <a onClick={handleChevron} id='source' href='#demo' data-toggle='collapse'>
-        <i class='chev fas fa-chevron-down'></i>&nbsp;&nbsp;SOURCE
-      </a>
+        <a
+          onClick={handleChevron}
+          id='source'
+          href='#demo'
+          data-toggle='collapse'
+        >
+          <i class='chev fas fa-chevron-down'></i>&nbsp;&nbsp;SOURCE
+        </a>
 
-      <div
-        style={{ marginTop: '10px', marginLeft: '20px' }}
-        id='demo'
-        className='collapse'
-      >
-        <Form>
-          {/* <h2>Filter by:</h2> */}
-          {/* <p className='filter'>
+        <div
+          style={{ marginTop: '10px', marginLeft: '20px' }}
+          id='demo'
+          className='collapse'
+        >
+          <Form>
+            {/* <h2>Filter by:</h2> */}
+            {/* <p className='filter'>
             <i class='fas fa-chevron-up'></i>
             TOPIC CLEAR
           </p> */}
-          {['radio'].map((type) => (
-            <div key={`custom-${type}`} className='mb-3'>
-              <Form.Check
-                onClick={handleUS}
-                custom
-                type='radio'
-                label='NEW YORK Times'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios1'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleWorld}
-                custom
-                type='radio'
-                label='Fox News'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios2'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handlePolitics}
-                custom
-                type='radio'
-                label='Washington Post'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios3'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleBusiness}
-                custom
-                type='radio'
-                label='BBC'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios4'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleTech}
-                custom
-                type='radio'
-                label='Al Jazeera'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios5'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleHealth}
-                custom
-                type='radio'
-                label='LA Times'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios6'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleSciEnv}
-                custom
-                type='radio'
-                label='Huffington Post'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios7'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleMedia}
-                custom
-                type='radio'
-                label='The Blaze'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios8'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handlePolls}
-                custom
-                type='radio'
-                label='CNN'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios9'
-                // className={`custom-${type}`}
-              />
-              <Form.Check
-                onClick={handleFaith}
-                custom
-                type='radio'
-                label='My Sources'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios10'
-                // className={`custom-${type}`}
-              />
-              
-              <Form.Check
-                onClick={handleSelectAll}
-                custom
-                type='radio'
-                label='Select All'
-                name='formHorizontalRadios'
-                id='formHorizontalRadios12'
-                // className={`custom-${type}`}
-              />
-            </div>
-            
-          ))}
-        </Form>
+            {['radio'].map((type) => (
+              <div key={`custom-${type}`} className='mb-3'>
+                <Form.Check
+                  onClick={handleUS}
+                  custom
+                  type='radio'
+                  label='NEW YORK Times'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios1'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleWorld}
+                  custom
+                  type='radio'
+                  label='Fox News'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios2'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handlePolitics}
+                  custom
+                  type='radio'
+                  label='Washington Post'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios3'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleBusiness}
+                  custom
+                  type='radio'
+                  label='BBC'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios4'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleTech}
+                  custom
+                  type='radio'
+                  label='Al Jazeera'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios5'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleHealth}
+                  custom
+                  type='radio'
+                  label='LA Times'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios6'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleSciEnv}
+                  custom
+                  type='radio'
+                  label='Huffington Post'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios7'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleMedia}
+                  custom
+                  type='radio'
+                  label='The Blaze'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios8'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handlePolls}
+                  custom
+                  type='radio'
+                  label='CNN'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios9'
+                  // className={`custom-${type}`}
+                />
+                <Form.Check
+                  onClick={handleFaith}
+                  custom
+                  type='radio'
+                  label='My Sources'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios10'
+                  // className={`custom-${type}`}
+                />
+
+                <Form.Check
+                  onClick={handleSelectAll}
+                  custom
+                  type='radio'
+                  label='Select All'
+                  name='formHorizontalRadios'
+                  id='formHorizontalRadios12'
+                  // className={`custom-${type}`}
+                />
+              </div>
+            ))}
+          </Form>
         </div>
-        
       </div>
       {/* <Row className='justify-content-md-center'>
           <Col xs lg='9'>
