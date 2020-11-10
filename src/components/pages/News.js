b51a21e8a59af499c198f0748c60a189e10ea5c2
Rose <rosier101@gmail.com>
2020-11-10 16:09:44 -0500

    Created News Page
    Created News.js file

---
+++ src/components/pages/News.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import FilterBar from '../layout/FilterBar';
+import NewsBoard from '../layout/ContentBoards/NewsBoard'
+
+
+
+class News extends React.Component {
+    render() {
+      return (
+       
+          <div>
+            <title>Our National Conversation | News</title>
+            <h1 className = "NewsTitle">NEWS</h1>
+        
+              <FilterBar />
+              <NewsBoard />
+              
+          </div>
+       
+      );
+    }
+  }
+
+  export default News;
