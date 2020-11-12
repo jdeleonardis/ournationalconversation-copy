0a3f9637258f4ade58d0de88ba7347a4f7d10a08
ANS <36783010+answebdev@users.noreply.github.com>
2020-11-11 19:10:16 -0800

    updated code

--- src/components/layout/ContentCards/TagLines.js
+++ src/components/layout/ContentCards/TagLines.js
@@ -5,26 +5,26 @@ import './syles/verticalcard.css';
 import GlobalButton from '../GlobalButton';
 
 const breakPoints = [
-	{ width: 1, itemsToShow: 1 },
-	{ width: 300, itemsToShow: 4, itemsToScroll: 2 },
-	{ width: 768, itemsToShow: 6 },
-	{ width: 1200, itemsToShow: 8 },
+  { width: 1, itemsToShow: 1 },
+  { width: 300, itemsToShow: 4, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 6 },
+  { width: 1200, itemsToShow: 8 },
 ];
 
 //TagLine Carouselll......
 function TagLine() {
-	return (
-		<div>
-			<Carousel breakPoints={breakPoints}>
-				<GlobalButton label={'CNN'} />
-				<GlobalButton label={'YEET'} />
-				<GlobalButton label={'NARUTO'} />
-				<GlobalButton label={'ASTROWORLD'} />
-				<GlobalButton label={'EGG'} />
-				<GlobalButton label={'CNN'} />
-			</Carousel>
-		</div>
-	);
+  return (
+    <div>
+      <Carousel breakPoints={breakPoints}>
+        <GlobalButton label={'CNN'} />
+        <GlobalButton label={'YEET'} />
+        <GlobalButton label={'NARUTO'} />
+        <GlobalButton label={'ASTROWORLD'} />
+        <GlobalButton label={'EGG'} />
+        <GlobalButton label={'CNN'} />
+      </Carousel>
+    </div>
+  );
 }
 
 export default TagLine;
