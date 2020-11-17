59c416245f56885bd4dd9887a7082749fead123a
Judy Lime <judylime9@gmail.com>
2020-11-16 21:04:36 -0800

    Make subhead-searchBar styles compatible with different browsers and screen types

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import {
-  Navbar,
-  Nav,
-  Col
-} from 'react-bootstrap';
+import { Navbar, Nav, Col } from 'react-bootstrap';
 import '../styles/searchBar.css';
-
 
 const SearchBar = (props) => {
   return (
-    <div>
-      <Navbar className='search-bar' variant=''>
-        <Col className='mb-2' xs lg='9'>
-          <Nav className='group-68 mr-auto '>
+    <>
+      <Navbar className='search-bar ' variant='' >
+        <Col className='mb-2' xs lg='3'>
+          <Navbar.Brand href='#'>
             <button className='news-filters-btn' href='#'>
-              NEWS FILTERS<i class='fas fa-chevron-down news-filters-btn' />
+              NEWS FILTERS
+              <i class='fas fa-chevron-down news-filters-btn' />
             </button>
-            <Nav.Link className='hot-topics' href='#'>
-              HOT TOPICS: 
+          </Navbar.Brand>
+        </Col>
+        <Col className='mb-2' xs lg='6'>
+          <Nav className='group-68 mr-auto '>
+            <Nav.Link className='hot-topics search-bar-text' href='#'>
+              HOT TOPICS:
             </Nav.Link>
             <Nav.Link className='search-bar-text' href='#'>
               COVID-19 Alert{props.hotTopics}
@@ -30,48 +30,14 @@ const SearchBar = (props) => {
             </Nav.Link>
           </Nav>
         </Col>
-        <Col lg='3' md='3'>
+        <Col className='mb-2' xs lg='3'>
           <input className='search' type='text' placeholder='SEARCH' />
           <i className='fa fa-search search-i' />
         </Col>
       </Navbar>
-      <hr/>
-    </div>
-  )
-}
-
-// class SearchBar extends Component {
-//   render() {
-//     return (
-
-//       <Navbar className='search-bar' variant=''>
-//         <Col className='mb-2' xs lg='9'>
-//           <Nav className='group-68 mr-auto '>
-//             <button className='news-filters-btn' href='#'>
-//               NEWS FILTERS <i class='fas fa-chevron-down news-filters-btn' />
-//             </button>
-//             <Nav.Link href='#'>
-//               <span className='hot-topics'>HOT TOPICS: </span>
-//             </Nav.Link>
-//             <Nav.Link className='search-bar-text' href='#'>
-//               COVID-19 Alert
-//             </Nav.Link>
-//             <Nav.Link className='search-bar-text' href='#'>
-//               Biden VP Pick
-//             </Nav.Link>
-//             <Nav.Link className='search-bar-text' href='#'>
-//               School Reopenings
-//             </Nav.Link>
-//           </Nav>
-//         </Col>
-//         <Col lg='3' md='auto'>
-//           <input className='search' type='text' placeholder='SEARCH' />
-//           <i className='fa fa-search search-i' />
-//         </Col>
-//       </Navbar>
-
-//     );
-//   }
-// }
+      <hr />
+    </>
+  );
+};
 
 export default SearchBar;
