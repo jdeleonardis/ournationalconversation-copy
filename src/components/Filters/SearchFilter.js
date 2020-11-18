50ae163e0a78a4c6b864f6b356412f36083cb832
Judy Lian <judylime9@gmail.com>
2020-11-18 14:38:50 -0800

    Merged FilerBar.js

--- src/components/Filters/SearchFilter.js
+++ src/components/Filters/SearchFilter.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import SearchBox from './SearchBox'
-import SortBy from './SortBy'
-import FilterBar from './FilterBar'
+import SearchBar from './SearchBar';
+import { Navbar, Nav, Row, Col, Container } from 'react-bootstrap';
+import SortBy from './SortBy';
+// import SortByLocal from './SortByLocal';
+// import SortByStore from './SortByStore';
+import FilterBar from './FilterBar';
+import SourceFilterBar from './SourceFilterBar';
 import RailButtons from '../Misc/RailButtons';
+import SearchBox from './SearchBox';
+// import AddLocation from "./AddLocation";
 
 class SearchFilter extends Component {
   render() {
     return (
-    <div>
+      <div>
         <Row>
           <Col md={3}>
-          <SearchBox/>
-          <FilterBar/>
+            <SearchBox />
+            <Col>
+              <FilterBar />
+              <SourceFilterBar />
+            </Col>
           </Col>
           <Col md={6}>
-            <SortBy/>
+            <SortBy />
+            {/* <SortByLocal /> */}
+            {/* <AddLocation/> */}
+            {/* <SortByStore /> */}
           </Col>
           <Col md={3}>
-            <RailButtons/>
+            <RailButtons />
           </Col>
         </Row>
-    </div>
+      </div>
     );
   }
 }
-export default SearchFilter;
 
+export default SearchFilter;
