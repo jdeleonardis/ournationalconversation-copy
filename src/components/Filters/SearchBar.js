4b5beef885e3b65ac0d2aaffd5d69e3a2682da3a
Judy Lian <judylime9@gmail.com>
2020-11-19 00:25:57 -0800

    Add Theming and customizing styles

--- src/components/Filters/SearchBar.js
+++ src/components/Filters/SearchBar.js
@@ -17,9 +17,44 @@ const SearchBar =(props) => {
       <Navbar className='search-bar' variant=''>
         <Col className='mb-2' xs lg='9'>
           <Nav className='group-68 mr-auto '>
-            <button className='news-filters-btn' href='#'>
-              NEWS FILTERS <i class='fas fa-chevron-down news-filters-btn' />
-            </button>
+            <div class='dropdown'>
+              <button
+                className='news-filters-btn '
+                type='button'
+                id='dropdownMenu2'
+                // data-toggle='dropdown'
+                // aria-haspopup='true'
+                aria-expanded='false'
+                data-toggle='collapse'
+                data-target='#collapseExample'
+                aria-controls='collapseExample'
+                href='#'
+              >
+                NEWS FILTERS <i class='fas fa-chevron-down news-filters-btn' />
+              </button>
+              <div class='collapse' id='collapseExample'>
+                <div class='card card-body'>
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life
+                  accusamus terry richardson ad squid. Nihil anim keffiyeh
+                  helvetica, craft beer labore wes anderson cred nesciunt
+                  sapiente ea proident.
+                </div>
+              </div>
+              <div class='dropdown-menu' aria-labelledby='dropdownMenu2'>
+                <button class='dropdown-item' type='button'>
+                  TOP STORIES
+                </button>
+                <button class='dropdown-item' type='button'>
+                  LOCAL STORIES
+                </button>
+                <button class='dropdown-item' type='button'>
+                  MY SAVED SEARCHES
+                </button>
+                <button class='dropdown-item' type='button'>
+                  MY SAVED NEWS
+                </button>
+              </div>
+            </div>
             <Nav.Link href='#'>
               <span className='hot-topics'>HOT TOPICS: </span>
             </Nav.Link>
@@ -38,11 +73,10 @@ const SearchBar =(props) => {
           <input className='search' type='text' placeholder='SEARCH' />
           <i className='fa fa-search search-i' />
         </Col>
-        
       </Navbar>
-      <hr/>
+      <hr />
     </>
-  )
+  );
 }
 
 
