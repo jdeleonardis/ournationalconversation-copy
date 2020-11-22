e65c0e7dab456fbf096b53846062d0ee2ef0c3fe
ANS <36783010+answebdev@users.noreply.github.com>
2020-11-22 13:55:54 -0800

    added star ratings component

--- src/components/Content/ContentCards/StoreCards.js
+++ src/components/Content/ContentCards/StoreCards.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import GlobalButton from '../../Misc/GlobalButton';
 import '../../../styles/StoreCards.css';
+import StarRatingComponent from 'react-star-rating-component';
 
 function StoreCards() {
   return (
@@ -9,34 +10,69 @@ function StoreCards() {
       <Container>
         <Row>
           <Col md={6}>
-            <Card style={{ width: '15rem', float: 'left' }}>
+            <Card className='book-card'>
               <Card.Img
                 className='book-image'
                 variant='top'
                 src='https://redshelf-images.s3-external-1.amazonaws.com/item_image/default_item_image.jpg'
               />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  BOOK <span className='book-price'>$13.95</span>
+                </Card.Subtitle>
+                <Card.Title>
+                  Rethinking America: Rethinking the World
+                </Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  {/* <div className='rating-stars'>
+                    <i class='fas fa-star'></i>&nbsp;&nbsp;
+                    <i class='fas fa-star'></i>&nbsp;&nbsp;
+                    <i class='fas fa-star'></i>&nbsp;&nbsp;
+                    <i class='fas fa-star'></i>&nbsp;&nbsp;
+                    <i class='fas fa-star'></i>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<span className='rating'>5.00</span>
+                  </div> */}
+                  <div style={{ marginLeft: '10px' }}>
+                    <StarRatingComponent
+                      name='rate2'
+                      editing={false}
+                      emptyStarColor={'#929292'}
+                      renderStarIcon={() => (
+                        <span style={{ margin: '0 5px', fontSize: '10px' }}>
+                          <i class='fas fa-star'></i>
+                        </span>
+                      )}
+                      starCount={5}
+                      value={4}
+                    />
+                    {/* <span className='rating'>5.00</span> */}
+                  </div>
+                </Card.Subtitle>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Our book, filled with 41 nonpartisan policy proposals + 8
+                  moonshot teasers.
                 </Card.Text>
                 <GlobalButton label='ADD TO CART' />
               </Card.Body>
             </Card>
           </Col>
           <Col md={6}>
-            <Card style={{ width: '15rem', float: 'right' }}>
+            <Card className='book-card'>
               <Card.Img
                 className='book-image'
                 variant='top'
                 src='https://redshelf-images.s3-external-1.amazonaws.com/item_image/default_item_image.jpg'
               />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  BOOK <span className='book-price'>$13.95</span>
+                </Card.Subtitle>
+                <Card.Title>
+                  Rethinking America: Rethinking the World
+                </Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Our book, filled with 41 nonpartisan policy proposals + 8
+                  moonshot teasers.
                 </Card.Text>
                 <GlobalButton label='ADD TO CART' />
               </Card.Body>
