9ecf95a927343f8078449630f9a0b20f7ab16ca8
ANS <36783010+answebdev@users.noreply.github.com>
2020-11-23 15:18:05 -0800

    added second row of book cards

--- src/components/Content/ContentCards/StoreCards.js
+++ src/components/Content/ContentCards/StoreCards.js
@@ -81,6 +81,67 @@ function StoreCards() {
             </Card>
           </Col>
         </Row>
+
+        <Row>
+          <Col md={6}>
+            <Card className='bottom-card'>
+              <Card.Img className='book-image' variant='top' src={bookCover} />
+              <Card.Body>
+                <Card.Subtitle className='mb-2 text-muted item-category'>
+                  BOOK <span className='book-price'>$13.95</span>
+                </Card.Subtitle>
+                <Card.Title className='book-title-text'>
+                  Let's Fix America
+                </Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  <div className='rating-stars'>
+                    <Star style={filledStar} />
+                    <Star style={filledStar} />
+                    <Star style={filledStar} />
+                    <StarHalf style={filledStar} />
+                    <Star style={star} />
+                    &nbsp;&nbsp;
+                    <span className='rating'>5.00</span>
+                  </div>
+                </Card.Subtitle>
+                <Card.Text className='book-caption'>
+                  Our book, filled with 41 nonpartisan policy proposals + 8
+                  moonshot teasers.
+                </Card.Text>
+                <GlobalButton float='right' label='ADD TO CART' />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={6}>
+            <Card className='bottom-card'>
+              <Card.Img className='book-image' variant='top' src={bookCover} />
+              <Card.Body>
+                <Card.Subtitle className='mb-2 text-muted item-category'>
+                  BOOK <span className='book-price'>$13.95</span>
+                </Card.Subtitle>
+                <Card.Title className='book-title-text'>
+                  Let's Fix America
+                </Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  <div className='rating-stars'>
+                    <Star style={filledStar} />
+                    <Star style={filledStar} />
+                    <Star style={filledStar} />
+                    <StarHalf style={filledStar} />
+                    <Star style={star} />
+                    &nbsp;&nbsp;
+                    <span className='rating'>5.00</span>
+                  </div>
+                </Card.Subtitle>
+                <Card.Text className='book-caption'>
+                  Our book, filled with 41 nonpartisan policy proposals + 8
+                  moonshot teasers.
+                </Card.Text>
+                <GlobalButton float='right' label='ADD TO CART' />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
