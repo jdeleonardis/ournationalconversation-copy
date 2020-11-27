711b18788ebcbac82afff85cca3ebaf49eb55c92
Judy Lian <judylime9@gmail.com>
2020-11-27 00:15:36 -0800

    Worked on Add location card

--- src/components/Content/ContentCards/AddLocationCart.js
+++ src/components/Content/ContentCards/AddLocationCart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Col, Row, Container } from 'react-bootstrap';
+import { Card, Button, Image, Col, Row, Container } from 'react-bootstrap';
 import './styles/comments.css';
 import CardBody from './CardBody';
 import TagLine from './TagLines';
@@ -9,96 +9,20 @@ import ClockSvg from './styles/clockSvg';
 
 function AddLocationCard(props) {
   return (
-    <div>
+    <>
       <Container>
-        <Card>
-          <Container>
-            <Row>
-              <Col md={props.vertical ? 12 : 2} sm={12}>
-                <span
-                  style={{
-                    fontWeight: 'bold',
-                    color: 'rgb(48 31 97)',
-                    fontSize: '25px'
-                  }}
-                >
-                  {' '}
-                  Sources{' '}
-                </span>
-              </Col>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant='' src='../../img/{props.title}' />
+          <Card.Body>
+  <Card.Title>Card Title{props.title}</Card.Title>
 
-              <Col md={props.vertical ? 12 : 10} sm={12}>
-                <TagLine />
-              </Col>
-            </Row>
-            <Row>
-              <Col md={12}>
-                <div className='passive'> NEWS | POLITICS</div>
-              </Col>
-              <Col md={12}>
-                <h1
-                  className='titleName'
-                  style={
-                    !props.vertical
-                      ? { fontSize: '25px' }
-                      : { fontSize: '20px' }
-                  }
-                >
-                  A New Clash Over Mail Voting: The Cost of Postage
-                </h1>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col xs={12} sm={12} md={props.vertical ? 12 : 4}>
-                <Card.Img
-                  src='https://c.ndtvimg.com/2020-10/el6r9e5c_xi-jinping-afp_625x300_31_October_20.jpg'
-                  alt='Card image'
-                />
-              </Col>
-              <Col xs={12} sm={12} md={props.vertical ? 12 : 8}>
-                <Card.Body>
-                  <Row>
-                    <p className='AddLocationCardText'>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book. It has survived not only five
-                      centuries, but also the leap into electronic typesetting,
-                      remaining essentially unchanged. It was popularised in the
-                      1960s with the release of Letraset sheets containing Lorem
-                      Ipsum passages, and more recently with desktop publishing
-                      software like Aldus PageMaker including versions of Lorem
-                      Ipsum.
-                    </p>
-                  </Row>
-                </Card.Body>
-              </Col>
-            </Row>
-            <br />
-            <Row>
-              <Col>
-                <span>
-                  <ClockSvg />
-                </span>
-                <span className='actionBtnTexts'> 4 min ago</span>
-              </Col>
-              <Col className='text-right'>
-                <span style={{ marginRight: '5%' }}>
-                  <ShareSvg />
-                </span>
-                <span style={{ marginRight: '2%' }}>
-                  <SaveSvg />
-                </span>
-                <br />
-              </Col>
-            </Row>
-            <br />
-          </Container>
+            <Button variant=''>
+              ADD <i class='fas fa-plus'> </i>
+            </Button>
+          </Card.Body>
         </Card>
       </Container>
-    </div>
+    </>
   );
 }
 
--- src/components/Filters/Overlay/LocationOverlay.js
+++ src/components/Filters/Overlay/LocationOverlay.js
@@ -1,8 +1,9 @@
 import React, { useState, Component } from 'react';
-import { Modal, Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Modal, Button, Container, Row, Col, Image ,} from 'react-bootstrap';
 import Overlay from 'react-bootstrap/Overlay';
 import '../../../styles/shoppingcart-overlay.css';
 import bookCover from '../../../img/book-cover.jpg';
+import AddLocationCard from '../../Content/ContentCards/AddLocationCart';
 
 const LocationOverlay = () => {
   const [show, setShow] = useState(false);
@@ -19,7 +20,6 @@ const LocationOverlay = () => {
       >
         ADD NEW LOCATION <i class='fas fa-plus'> </i>{' '}
       </Button>
-      
 
       <Modal
         show={show}
@@ -36,81 +36,38 @@ const LocationOverlay = () => {
             </Row>
             <div className='add-location-table'>
               <Row className='add-location-items'>
-                <input id='small-search-field' type='text' placeholder='Covid'></input>
-        <i id='searchsubmit' className='fa fa-search'></i>
-      
+                <input
+                  id='small-search-field'
+                  type='text'
+                  placeholder='Covid'
+                ></input>
+                <i id='searchsubmit' className='fa fa-search'></i>
               </Row>
-              <hr className='rectangle-37' />
-              <Row className='add-location-details'>
-                <Col xs={4} md={2}>
-                  <Image
-                    className='add-location-img'
-                    variant='top'
-                    src={bookCover}
-                    fluid
-                  ></Image>
+              <Row>
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
                 </Col>
-                <Col className='add-location-des' xs={6} md={4}>
-                  <span>Rethinking America: Rethinking the World</span>
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
                 </Col>
-                <Col className='add-location-qty' xs={4} md={3}>
-                  <i class='fas fa-minus' />
-                  <span> 1 </span>
-                  <i class='fas fa-plus' />
-                </Col>
-                <Col className='add-location-price' xs={4} md={3}>
-                  <span className='col-center'>$13.95</span>
-                </Col>
-              </Row>
-              <hr className='line-86' />
-              <Row className='subtotal'>
-                <Col className='add-location-sub' xs={4} md={9}>
-                  SUBTOTAL
-                </Col>
-
-                <Col className='add-location-price price' xs={6} md={3}>
-                  <span>$13.95</span>
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
                 </Col>
               </Row>
               <Row>
-                <Col className='add-location-shipping' xs={4} md={6}>
-                  {' '}
-                  SHIPPING{' '}
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
                 </Col>
-
-                <Col className='add-location-caption' xs={4} md={6}>
-                  <p>Shipping calculated at checkout</p>
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
+                </Col>
+                <Col xs={6} md={4}>
+                  <AddLocationCard />
                 </Col>
               </Row>
             </div>
           </Container>
-          <Container className='add-location-btn'>
-            <Button className='add-location-checkout' variant=''>
-              CHECKOUT
-            </Button>
-            <div>
-              <Row>
-                <Col md={5}>
-                  <div className='' style={{ width: '100%' }}>
-                    <div id=''>&nbsp;&nbsp;</div>
-                    <div className='line-or'></div> &nbsp;&nbsp;&nbsp;
-                  </div>
-                </Col>
-                <Col md={2}>
-                  <p className='add-location-or'>OR</p>
-                </Col>
-                <Col md={5}>
-                  <div className='' style={{ width: '100%' }}>
-                    <div id=''>&nbsp;&nbsp;</div>
-                    <div className='line-or'></div> &nbsp;&nbsp;&nbsp;
-                  </div>
-                </Col>
-              </Row>
-            </div>
-            <Button className='add-location-cont' variant=''>
-              CONITUE SHOPPING
-            </Button>
-          </Container>
+          
         </Modal.Body>
       </Modal>
     </>
