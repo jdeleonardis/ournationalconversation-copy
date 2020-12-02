a5537a877a7b8dbc5a6db9929f320b105c8dea73
Judy Lian <judylime9@gmail.com>
2020-12-01 21:21:24 -0800

    Add images to Add location Card

--- src/components/Content/ContentCards/AddLocationCard.js
+++ src/components/Content/ContentCards/AddLocationCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button,Col} from 'react-bootstrap';
+import { Card, Button,Col,Row,Image} from 'react-bootstrap';
 
 import Nashville from '../../../img/Nashville.jpg';
 import Houston from '../../../img/Houston.jpg';
@@ -7,7 +7,7 @@ import Detroit from '../../../img/Detroit.jpg';
 import Chicago from '../../../img/Chicago.jpg';
 import Denver from '../../../img/Denver.jpg';
 import Portland from '../../../img/Portland.jpg';
-import bookCover from '../../../img/book-cover.jpg';
+
 
 import '../../../styles/LocationOverlay.css';
 
@@ -15,9 +15,36 @@ import '../../../styles/LocationOverlay.css';
 function AddLocationCard(props) {
   return (
     <>
+      {/*       
+        <Col xs={6} md={4}>
+          <Image src='holder.js/171x180' rounded />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src='holder.js/171x180' roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src='holder.js/171x180' thumbnail />
+        </Col>
+   */}
+
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Nashville}'>
+          <div className="frontground"></div>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Nashville}
+            // width='100'
+            // height='75'
+            // alt='Nashville [100%x75]
+            // '
+          >
+            {/* <Card.Img
+            className='local-pic'
+            variant=''
+            src='/img/Nashville.jpg'
+           alt='Nashville[100%x75]'
+          > */}
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
@@ -32,7 +59,13 @@ function AddLocationCard(props) {
       </Col>
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Houston}'>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Houston}
+            width='100'
+            height='75'
+          >
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
@@ -47,7 +80,13 @@ function AddLocationCard(props) {
       </Col>
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Detroit}'>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Detroit}
+            width='100'
+            height='75'
+          >
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
@@ -62,7 +101,13 @@ function AddLocationCard(props) {
       </Col>
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Chicago}'>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Chicago}
+            width='100'
+            height='75'
+          >
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
@@ -77,7 +122,13 @@ function AddLocationCard(props) {
       </Col>
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Denver}'>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Denver}
+            width='100'
+            height='75'
+          >
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
@@ -90,7 +141,13 @@ function AddLocationCard(props) {
       </Col>
       <Col xs={6} md={4}>
         <Card className='location-card'>
-          <Card.Img className='local-pic' variant='' src='{Portland}'>
+          <Card.Img
+            className='local-pic'
+            variant=''
+            src={Portland}
+            width='100'
+            height='75'
+          >
             {props.local}
           </Card.Img>
           <Card.Body className='local-name' variant=''>
