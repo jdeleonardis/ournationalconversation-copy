eb6b1a6d8dfd238557e19981cfa740f8b423ff81
Judy Lian <judylime9@gmail.com>
2020-12-01 19:23:21 -0800

    Added Local name styling to Add Location Card

--- src/components/Content/ContentCards/AddLocationCard.js
+++ src/components/Content/ContentCards/AddLocationCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Card, Button} from 'react-bootstrap';
+import { Card, Button,Col} from 'react-bootstrap';
+
+import Nashville from '../../../img/Nashville.jpg';
+import Houston from '../../../img/Houston.jpg';
+import Detroit from '../../../img/Detroit.jpg';
 import Chicago from '../../../img/Chicago.jpg';
 import Denver from '../../../img/Denver.jpg';
-import Detroit from '../../../img/Detroit.jpg';
-import Houston from '../../../img/Houston.jpg';
-import Nashville from '../../../img/Nashville.jpg';
 import Portland from '../../../img/Portland.jpg';
+import bookCover from '../../../img/book-cover.jpg';
 
 import '../../../styles/LocationOverlay.css';
 
@@ -13,23 +15,94 @@ import '../../../styles/LocationOverlay.css';
 function AddLocationCard(props) {
   return (
     <>
-      <Card className='location-card'>
-        <Card.Img
-          className='local-pic'
-          variant=''
-          // src='{props.local}/100px180?text={props.local}'
-        >
-          {props.local}
-        </Card.Img>
-
-        <Card.Body className='local-name' variant=''>
-          <Card.Title>Local{props.local}</Card.Title>
-
-          <Button className='local-add' variant=''>
-            ADD <i class='fas fa-plus' />
-          </Button>
-        </Card.Body>
-      </Card>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Nashville}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>
+              Nashville{props.local}
+            </Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Houston}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>
+              Houston{props.local}
+            </Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Detroit}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>
+              Detroit{props.local}
+            </Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Chicago}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>
+              Chicago{props.local}
+            </Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Denver}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>Denver{props.local}</Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col xs={6} md={4}>
+        <Card className='location-card'>
+          <Card.Img className='local-pic' variant='' src='{Portland}'>
+            {props.local}
+          </Card.Img>
+          <Card.Body className='local-name' variant=''>
+            <Card.Title className='local-title'>
+              Portland{props.local}
+            </Card.Title>
+            <Button className='local-add' variant=''>
+              ADD <i class='fas fa-plus' />
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
     </>
   );
 }
