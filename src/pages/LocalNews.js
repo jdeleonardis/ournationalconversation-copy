467648451ac8b10be42a415721c0b6fa23bd34d4
Judy Lian <judylime9@gmail.com>
2020-12-01 22:27:31 -0800

    Added Components to Local News Page

--- src/pages/LocalNews.js
+++ src/pages/LocalNews.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import { Helmet } from 'react-helmet';
-import FilterBar from '../components/Filters/FilterBar';
-import NewsCard from '../components/Content/ContentCards/NewsCard';
 import { Container, Row, Col } from 'react-bootstrap';
+
+import NewsCard from '../components/Content/ContentCards/NewsCard';
+import BreakingNewsCard from '../components/Content/ContentCards/BreakingNewsCard';
 import SortByLocal from '../components/Filters/SortByLocal';
 import RailButtons from '../components/Misc/RailButtons';
 import GlobalButton from '../components/Misc/GlobalButton';
-import SearchBox from '../components/Filters/SearchBox';
+import OpinionBoard from '../components/Content/ContentBoards/ContentBoard';
 import TopHorizontalLine from '../components/Misc/TopHorizontalLine';
 
 const Home = () => {
@@ -24,10 +25,58 @@ const Home = () => {
               <SortByLocal />
               <br />
               <NewsCard />
+              <br />
+              <Row>
+                <Col>
+                  <BreakingNewsCard />
+                </Col>
+                <Col>
+                  <BreakingNewsCard />
+                </Col>
+                <Col>
+                  <BreakingNewsCard />
+                </Col>
+              </Row>
             </Col>
             <Col md={3}>
               <RailButtons />
+
+              <div
+                style={{
+                  marginLeft: '0em',
+                  marginTop: '40px',
+                  marginBottom: '-10px'
+                }}
+              >
+                <TopHorizontalLine lineLabel='OPINIONS' />
+              </div>
+              <br />
+              <BreakingNewsCard />
+              <br />
+              <Row>
+                <GlobalButton
+                  className='global-btn'
+                  padding='0.5em 1em'
+                  margin='2em'
+                  fontWeight='700'
+                  label='SEE MORE OPINIONS'
+                />
+              </Row>
             </Col>
+          </Row>
+          <br />
+          <Row>
+            <br />
+            <Col></Col>
+            <Col>
+              <GlobalButton
+                className='global-btn'
+                padding='7px 57.5px'
+                fontWeight='700'
+                label='SEE MORE NEWS'
+              />
+            </Col>
+            <Col></Col>
           </Row>
 
           <Row>
@@ -39,7 +88,9 @@ const Home = () => {
           </Row>
 
           <Row>
-            <Col md={9}></Col>
+            <Col md={9}>
+              {/* <OpinionBoard type={type.SOLUTION} data={data} /> */}
+            </Col>
             <Col md={3}></Col>
           </Row>
         </Container>
