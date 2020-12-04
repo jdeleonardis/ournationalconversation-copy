a25497680741028c7d73392222a09fa85146e7a8
rosier101 <rosier101@gmail.com>
2020-12-04 14:05:49 -0500

    Testing customerinfo

--- src/components/Content/ContentCards/CustomerInfo.js
+++ src/components/Content/ContentCards/CustomerInfo.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import NewsCard from '../components/Content/ContentCards/NewsCard';
 
 
 class CustomerInfo extends React.Component {
- 
-  
-}
+    render() {
+        return (
 
+    <div>
+    <Helmet>
+      <title>Our National Conversation | News</title>
+    </Helmet>
+    <NewsCard />
+    <h1>CHECKOUT!</h1>
+    </div>
+
+
+);
+}
+}
 export default CustomerInfo;
