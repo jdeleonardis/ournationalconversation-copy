f182b138bee7448c92095fad60f9625cd8397da1
Rose <rosier101@gmail.com>
2020-12-04 19:03:42 -0500

    Customer file
    Customer file

---
+++ src/components/Content/ContentCards/Customer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+class Customer extends React.Component {
+    render() {
+        return (
+
+    <div>
+    <h1>CHECKOUT!</h1>
+    <p>Testing out this page!</p>
+    </div>
+
+
+);
+}
+}
+export default Customer;
