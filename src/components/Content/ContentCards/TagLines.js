99b9a44e1a4cfb8daf84b98f400413c0da1dc1f1
rjvarona <rjvarona16@gmail.com>
2020-12-07 16:32:12 -0800

    modifying taglines

--- src/components/Content/ContentCards/TagLines.js
+++ src/components/Content/ContentCards/TagLines.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
 import Carousel from 'react-elastic-carousel';
 import GlobalButton from '../../Misc/GlobalButton';
 import '../ContentCards/styles/tagline.css';
 
 const breakPoints = [
 	{ width: 1, itemsToShow: 1 },
-	{ width: 300, itemsToShow: 4, itemsToScroll: 2 },
-	{ width: 768, itemsToShow: 4 },
-	{ width: 1200, itemsToShow: 4 },
+	{ width: 300, itemsToShow: 5 },
+	{ width: 768, itemsToShow: 5 },
+	{ width: 1200, itemsToShow: 6 },
 ];
 
 function TagLine() {
 	return (
 		<div className="styling-example">
 			<Carousel breakPoints={breakPoints}>
-				<GlobalButton label={'CNN'} />
-				<GlobalButton label={'YEET'} />
-				<GlobalButton label={'NARUTO'} />
-				<GlobalButton label={'EGG'} />
-				<GlobalButton label={'CNN'} />
+				<GlobalButton
+					label={'CNN'}
+					fontSize={'12px'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					display={'flex'}
+				/>
+				<GlobalButton
+					label={'YEET'}
+					fontSize={'12px'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					width={'110%'}
+				/>
+				<GlobalButton
+					label={'NARUTO'}
+					fontSize={'12px'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					display={'flex'}
+					height={'60%'}
+					width={'90%'}
+				/>
+				<GlobalButton
+					label={'NARUTO'}
+					fontSize={'12px'}
+					height={'80%'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					display={'flex'}
+					width={'90%'}
+				/>
+				<GlobalButton
+					label={'EGG'}
+					fontSize={'12px'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					display={'flex'}
+				/>
+				<GlobalButton
+					label={'CNN'}
+					fontSize={'12px'}
+					justifyContent={'center'}
+					alignItems={'center'}
+					display={'flex'}
+					width={'110%'}
+				/>
 			</Carousel>
 		</div>
 	);
--- src/components/Misc/GlobalButton.js
+++ src/components/Misc/GlobalButton.js
@@ -2,25 +2,27 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import '../../styles/global-btn.css';
 
-const GlobalButton = (props) => {
-  return (
-    <div>
-      <Button
-        className='global-btn '
-        href={props.link}
-        style={{
-          width: `${props.width}`,
-          fontSize: `${props.fontSize}`,
-          margin: `${props.margin}`,
-          padding: `${props.padding}`,
-          float: `${props.float}`,
-        }}
-        variant='outline-primary'
-      >
-        {props.label}
-      </Button>
-    </div>
-  );
+const GlobalButton = props => {
+	return (
+		<Button
+			className="global-btn "
+			href={props.link}
+			style={{
+				justifyContent: `${props.justifyContent}`,
+				alignItems: `${props.alignItems}`,
+				display: `${props.display}`,
+				width: `${props.width}`,
+				fontSize: `${props.fontSize}`,
+				height: `${props.height}`,
+				margin: `${props.margin}`,
+				padding: `${props.padding}`,
+				float: `${props.float}`,
+			}}
+			variant="outline-primary"
+		>
+			{props.label}
+		</Button>
+	);
 };
 
 export default GlobalButton;
