c428b778822378f0dd3cbfff8638d28b024cef27
ANS <36783010+answebdev@users.noreply.github.com>
2020-12-08 18:22:25 -0800

    replaced Conact dropdown and added useState hook

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,122 +1,104 @@
-import React, { Component, Fragment } from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  DropdownButton,
-  Dropdown,
-  Form,
-  Button,
-  Card,
-} from 'react-bootstrap';
+import React, { Fragment } from 'react';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
+import subjects from '../components/Misc/ContactSubjects';
 import RailButtons from '../components/Misc/RailButtons';
 import '../styles/contact.css';
 
-export class Contact extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div>
-          <Helmet>
-            <title>Our National Conversation | Contact</title>
-          </Helmet>
+// export class Contact extends Component {
+//   render() {
 
-          <Container className='container' fluid>
-            <br />
+const Contact = () => {
+  const [subject, setSubject] = React.useState('');
+  return (
+    <Fragment>
+      <div>
+        <Helmet>
+          <title>Our National Conversation | Contact</title>
+        </Helmet>
 
-            <Row>
-              {/* <Col md={3}></Col> */}
-              <Col md={9}>
-                <div>
-                  <Card id='contact-card'>
-                    <Card.Body>
-                      <Card.Title id='contact-card-title'>CONTACT</Card.Title>
-                      <Card.Subtitle id='login-card-subtitle' className='mb-2'>
-                        <div id='contact-subtitle'>
-                          We’d love to hear from you! Send us a question or
-                          comment and we’ll be in touch as soon as possible.
-                        </div>
-                      </Card.Subtitle>
-                      <Form>
-                        <Form.Group controlId='formBasicEmail'>
-                          <Form.Label className='label'>SUBJECT</Form.Label>
-                          <DropdownButton
-                            id='dropdown-basic-button'
-                            title='SELECT SUBJECT'
-                          >
-                            <Dropdown.Item href='#/action-1'>
-                              EDITORIAL MATTERS
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-2'>
-                              BUSINESS PROPOSITIONS
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-3'>
-                              ADVERTISING INQUIRIES
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-4'>
-                              LEGAL ISSUES
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-5'>
-                              EMPLOYMENT INQUIRIES
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-6'>
-                              EMERGENCIES
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-7'>
-                              FOR QUESTIONS OR COMMENTS ABOUT OUR WEBISODES OR
-                              VIDEOS
-                            </Dropdown.Item>
-                            <Dropdown.Item href='#/action-8'>
-                              FOR GENERAL INQUIRIES OR COMMENTS
-                            </Dropdown.Item>
-                          </DropdownButton>
-                        </Form.Group>
-                        <Form.Group controlId='formBasicName'>
-                          <Form.Label className='label'>YOUR NAME</Form.Label>
-                          <Form.Control
-                            id='name-input'
-                            placeholder='FIRST AND LAST NAME'
-                            type='name'
-                            required
-                          />
-                        </Form.Group>
-                        <Form.Group controlId='formBasicEmail'>
-                          <Form.Label className='label'>
-                            YOUR EMAIL ADDRESS
-                          </Form.Label>
-                          <Form.Control
-                            id='email-input'
-                            placeholder='EMAIL'
-                            type='email'
-                            required
-                          />
-                        </Form.Group>
-                        <Form.Group controlId='exampleForm.ControlTextarea1'>
-                          <Form.Label className='label'>
-                            YOUR MESSAGE
-                          </Form.Label>
-                          <Form.Control as='textarea' rows='6' required />
-                        </Form.Group>
-                        <Button id='send-btn' variant='primary' type='submit'>
-                          SEND MESSAGE
-                        </Button>
-                      </Form>
-                    </Card.Body>
-                  </Card>
-                </div>
-              </Col>
-              <Col md={3}>
-                <div className='rail-group' style={{ marginTop: '-4px' }}>
-                  <RailButtons />
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </Fragment>
-    );
-  }
-}
+        <Container className='container' fluid>
+          <br />
+
+          <Row>
+            {/* <Col md={3}></Col> */}
+            <Col md={9}>
+              <div>
+                <Card id='contact-card'>
+                  <Card.Body>
+                    <Card.Title id='contact-card-title'>CONTACT</Card.Title>
+                    <Card.Subtitle id='login-card-subtitle' className='mb-2'>
+                      <div id='contact-subtitle'>
+                        We’d love to hear from you! Send us a question or
+                        comment and we’ll be in touch as soon as possible.
+                      </div>
+                    </Card.Subtitle>
+                    <Form>
+                      <Form.Group controlId='formBasicEmail'>
+                        <Form.Label className='label'>SUBJECT</Form.Label>
+
+                        <label id='dropdown-label'>
+                          <div className='input-div'>
+                            <select
+                              id='dropdown'
+                              name='subjects'
+                              value={subject}
+                              onChange={(e) => setSubject(e.target.value)}
+                              required
+                            >
+                              <option key=''>SELECT SUBJECT</option>
+                              {subjects.map((subject) => (
+                                <option key={subject}>{subject}</option>
+                              ))}
+                            </select>
+                            <span className='icon-span '>
+                              <span className='contact-caret-con fas fa-chevron-down fa-lg'></span>
+                            </span>
+                          </div>
+                        </label>
+                      </Form.Group>
+                      <Form.Group controlId='formBasicName'>
+                        <Form.Label className='label'>YOUR NAME</Form.Label>
+                        <Form.Control
+                          id='name-input'
+                          placeholder='FIRST AND LAST NAME'
+                          type='name'
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group controlId='formBasicEmail'>
+                        <Form.Label className='label'>
+                          YOUR EMAIL ADDRESS
+                        </Form.Label>
+                        <Form.Control
+                          id='email-input'
+                          placeholder='EMAIL'
+                          type='email'
+                          required
+                        />
+                      </Form.Group>
+                      <Form.Group controlId='exampleForm.ControlTextarea1'>
+                        <Form.Label className='label'>YOUR MESSAGE</Form.Label>
+                        <Form.Control as='textarea' rows='6' required />
+                      </Form.Group>
+                      <Button id='send-btn' variant='primary' type='submit'>
+                        SEND MESSAGE
+                      </Button>
+                    </Form>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div className='rail-group' style={{ marginTop: '-4px' }}>
+                <RailButtons />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Contact;
