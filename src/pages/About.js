5bf2b1f18442a3c59775e9db86f9c28f859ad2e0
rjvarona <rjvarona16@gmail.com>
2020-12-15 15:07:22 -0800

    added about page styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,34 +20,44 @@ export class Webisodes extends Component {
 						<Row>
 							<Col md={9}>
 								<div class="theBox">
-									<h1>ABOUT US</h1>
-									<h2 style={{ fontWeight: 'bold' }}>LOREM IPSUM YEET</h2>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-										a congue erat. Vestibulum ante ipsum primis in faucibus orci
-										luctus et ultrices posuere cubilia curae; Nullam porta
-										fringilla odio, a mattis arcu gravida ac. Vivamus orci
-										sapien, dignissim sit amet luctus sit amet, pellentesque
-										lobortis nisl. Donec eu magna tincidunt metus ornare maximus
-										quis in tortor. In a dapibus quam. Suspendisse porta libero
-										a libero commodo, nec porta arcu laoreet. Maecenas vitae
-										molestie turpis. Suspendisse auctor elementum nisl, in
-										mollis nisl venenatis sit amet. In non massa eget urna
-										posuere sagittis sed id massa. Etiam imperdiet nibh vel elit
-										ornare, in tincidunt purus aliquet. Ut venenatis, velit et
-										ornare sagittis, nunc mauris ullamcorper ligula, ut vehicula
-										sapien dui vitae purus. Sed fringilla velit semper lectus
-										gravida, sed interdum dolor ultrices. Donec vel magna eget
-										nibh finibus maximus et vel quam. Curabitur congue elementum
-										mattis. Pellentesque vitae aliquet arcu. Proin cursus diam
-										nulla, id finibus nisi dictum vitae. Proin a rhoncus sem.
-										Interdum et malesuada fames ac ante ipsum primis in
-										faucibus. Praesent finibus hendrerit tincidunt. Integer sed
-										varius massa. Vestibulum facilisis ligula sapien, facilisis
-										ullamcorper dui sodales sed. Phasellus vitae viverra sem, in
-										pretium dui. Aenean auctor, sem ut scelerisque mollis, purus
-										tellus rhoncus leo, at consequat justo lorem sit amet sem.
-									</p>
+									<div style={{ marginRight: '3rem', marginLeft: '3rem' }}>
+										<br />
+										<br />
+										<br />
+										<h1>ABOUT US</h1>
+										<h2 style={{ fontWeight: 'bold' }}>LOREM IPSUM YEET</h2>
+										<p>
+											Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+											Nam a congue erat. Vestibulum ante ipsum primis in
+											faucibus orci luctus et ultrices posuere cubilia curae;
+											Nullam porta fringilla odio, a mattis arcu gravida ac.
+											Vivamus orci sapien, dignissim sit amet luctus sit amet,
+											pellentesque lobortis nisl. Donec eu magna tincidunt metus
+											ornare maximus quis in tortor. In a dapibus quam.
+											Suspendisse porta libero a libero commodo, nec porta arcu
+											laoreet. Maecenas vitae molestie turpis. Suspendisse
+											auctor elementum nisl, in mollis nisl venenatis sit amet.
+											In non massa eget urna posuere sagittis sed id massa.
+											Etiam imperdiet nibh vel elit ornare, in tincidunt purus
+											aliquet. Ut venenatis, velit et ornare sagittis, nunc
+											mauris ullamcorper ligula, ut vehicula sapien dui vitae
+											purus. Sed fringilla velit semper lectus gravida, sed
+											interdum dolor ultrices. Donec vel magna eget nibh finibus
+											maximus et vel quam. Curabitur congue elementum mattis.
+											Pellentesque vitae aliquet arcu. Proin cursus diam nulla,
+											id finibus nisi dictum vitae. Proin a rhoncus sem.
+											Interdum et malesuada fames ac ante ipsum primis in
+											faucibus. Praesent finibus hendrerit tincidunt. Integer
+											sed varius massa. Vestibulum facilisis ligula sapien,
+											facilisis ullamcorper dui sodales sed. Phasellus vitae
+											viverra sem, in pretium dui. Aenean auctor, sem ut
+											scelerisque mollis, purus tellus rhoncus leo, at consequat
+											justo lorem sit amet sem.
+										</p>
+										<br />
+										<br />
+										<br />
+									</div>
 								</div>
 							</Col>
 							<Col md={3}>
@@ -58,7 +68,7 @@ export class Webisodes extends Component {
 							<div>
 								<Col md={9}>
 									<Row>
-										&nbsp;&nbsp;
+										&nbsp;&nbsp;&nbsp;
 										<div className="texttitle">MEET OUR MANAGEMENT TEAM</div>
 										&nbsp;&nbsp;
 										<div className="dividerAbout"></div>&nbsp;&nbsp;&nbsp;
